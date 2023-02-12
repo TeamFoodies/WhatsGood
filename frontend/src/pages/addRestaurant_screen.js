56279c7615f39ed2789e96d8e0cc95aa2d1a8c9d
Linda Ly <lly022@ucr.edu>
2023-02-11 19:13:38 -0800

    Add page title and Name entry

--- frontend/src/pages/addRestaurant_screen.js
+++ frontend/src/pages/addRestaurant_screen.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import PropTypes from 'prop-types';
 
 const Box = ({ children }) => (
@@ -20,6 +20,18 @@ export default function App() {
        <Box>
             <Text style={styles.innerText}> What's Good?</Text>
        </Box>
+
+       <View style={styles.title_container}>
+            <Text style={{fontSize:30}}> Add A Restuarant</Text>
+       </View>
+
+       <View style={styles.entries_layout}>
+            <Text style={styles.entries_titles}> Name: </Text>
+            <TextInput style={styles.entryInput} placeholder="Restaurant Name..." />
+       </View>
+
+       
+
     </View>
   );
 }
@@ -36,10 +48,34 @@ const styles = StyleSheet.create({
     borderColor: '#C4DAC2',
     padding: 10,
     margin: 0,
+    marginBottom: 20,
   },
   innerText: {
     fontWeight: 'bold',
     color: '#fff',
     fontSize: '50',
+  },
+  title_container: {
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  entries_layout: {
+    flexDirection: 'row',
+    marginLeft: 20,
+    marginBottom: 30,
+  },
+  entries_titles:{
+    fontSize: 24,
+    marginTop: 10,
+  },
+  entryInput:{
+    borderWidth: 1,
+    borderColor: 'black',
+    width: '70%',
+    marginTop: 10,
+    marginLeft: 10,
+    padding: 8,
+    marginBottom: 10,
+    marginTop: 10,
   },
 });

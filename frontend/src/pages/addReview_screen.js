aceb14d3ace20c064054a263133c23b0d02fdf7e
Linda Ly <lly022@ucr.edu>
2023-02-12 00:08:35 -0800

    Update addReview page

---
+++ frontend/src/pages/addReview_screen.js
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+

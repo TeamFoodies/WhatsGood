1bbe0725a3532179bf1eea2a884abafd365877e1
Linda Ly <lly022@ucr.edu>
2023-02-15 18:16:44 -0800

    Create view restaurant screen, add skeleton code

---
+++ frontend/src/pages/viewRestaurant_screen.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default function App() {
+  return (
+    <View style={styles.app_container}>
+       
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  app_container: {
+    flex: 1,
+    backgroundColor: '#959889',
+  },
+  
+});

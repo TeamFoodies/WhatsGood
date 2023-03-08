752c3b0007cffcd54b9f8332a10183eded5fee67
Justin <justin@justinf.dev>
2023-03-07 17:58:16 -0800

    Create authorization model

---
+++ backend/src/models/authorization.model.js
@@ -0,0 +1,20 @@
+const KEY_TIMEOUT_MS = 1;
+
+// Represents an authenticated user
+module.exports = class Authorization {
+  constructor(auth_key, username) {
+    this.auth_key = auth_key;
+    this.username = username;
+    this.timeout = Date.now() + KEY_TIMEOUT_MS;
+  }
+
+  // With any activity of the user, move the timeout expiration to later
+  bump() {
+    this.timeout = Date.now() + KEY_TIMEOUT_MS;
+  }
+
+  // Returns whether the authorization has timed out
+  isActive() {
+    return Date.now() < this.timeout;
+  }
+}

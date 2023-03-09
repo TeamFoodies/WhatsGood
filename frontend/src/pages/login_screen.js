d165450ab77164abe23deaad6872131ab28511ac
Steven Hoang <hoangste22@gmail.com>
2023-03-08 17:37:37 -0800

    took our forgot pass button

--- frontend/src/pages/login_screen.js
+++ frontend/src/pages/login_screen.js
@@ -9,9 +9,6 @@ export default function App({ navigation }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleForgotPassword = () => {
-    navigation.navigate("ForgotPasswordScreen");
-  };
 
   const handleLogin = () => {
     navigation.navigate("HomeScreen");
@@ -42,9 +39,6 @@ export default function App({ navigation }) {
           onChangeText={(password) => setPassword(password)}
         />
       </View>
-      <TouchableOpacity onPress={handleForgotPassword}>
-        <Text style={styles.forgot_button}>Forgot Password?</Text> 
-      </TouchableOpacity> 
       <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('home_screen')}>
         <Text style={styles.loginText}>LOGIN</Text> 
       </TouchableOpacity> 
@@ -82,10 +76,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     marginLeft: 20,
-  },
-  forgot_button: {
-    height: 30,
-    marginBottom: 30,
   },
   btn: {
     width: "80%",

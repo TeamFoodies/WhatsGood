879b8858343800f7f611432dfbc3a1b15409e37f
Justin <justin@justinf.dev>
2023-03-14 14:48:03 -0700

    Base favorite route

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,11 @@ const express = require('express');
 const index_route = require('./routes/index.route');
 const create_account_route = require('./routes/create_account.route');
 const login_route = require('./routes/login.route');
-const create_restaurant_route = require('./routes/restaurant.route');
+const restaurant_route = require('./routes/restaurant.route');
 const logout_route = require('./routes/logout.route');
 const review_route = require('./routes/review.route');
 const user_route = require('./routes/user.route');
+const favorite_route = require('./routes/favorite.route');
 
 const app = express();
 app.use(express.json());
@@ -23,10 +24,11 @@ const PORT = 4000;
 app.use('/', index_route);
 app.use('/create_account', create_account_route);
 app.use('/login', login_route);
-app.use('/restaurant', create_restaurant_route);
+app.use('/restaurant', restaurant_route);
 app.use('/logout', logout_route);
 app.use('/review', review_route);
 app.use('/user', user_route);
+app.use('/favorite', favorite_route);
 
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
---
+++ backend/src/routes/favorite.route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const Joi = require('joi');
+const router = express.Router();
+
+router.post('/add', (request, response) => {
+
+})
+
+router.post('/remove', (request, response) => {
+
+})
+
+module.exports = router;

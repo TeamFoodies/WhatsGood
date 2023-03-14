ef84d3e9fe8a4778d9b7d2f51090b7535942cf2c
Justin <justin@justinf.dev>
2023-03-14 13:08:32 -0700

    Stop using https

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,7 @@ require('dotenv').config();
 // Initialize mongo connection - close application if no connection string specified
 if (!require('./controllers/database.controller').mongo()) process.exit(1);
 
-// Starter code: https://adamtheautomator.com/https-nodejs/
-
-// Dependencies
-const https = require('https');
 const express = require('express');
-const fs = require('fs');
 
 // Routes
 const index_route = require('./routes/index.route');
@@ -23,17 +18,6 @@ const user_route = require('./routes/user.route');
 const app = express();
 app.use(express.json());
 const PORT = 4000;
-https
-  .createServer(
-    {
-      key: fs.readFileSync('ssl/key.pem'),
-      cert: fs.readFileSync('ssl/cert.pem'),
-    },
-    app
-  )
-  .listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  });
 
 // Routes
 app.use('/', index_route);
@@ -43,3 +27,6 @@ app.use('/restaurant', create_restaurant_route);
 app.use('/logout', logout_route);
 app.use('/review', review_route);
 app.use('/user', user_route);
+
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);

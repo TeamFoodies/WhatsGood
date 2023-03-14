92cfc717c554ab5966a9fb61c5f79ae4291d7473
Damian Ramos <damianramos00@gmail.com>
2023-03-14 12:36:40 -0700

    Uncomment lines

--- frontend/App.js
+++ frontend/App.js
@@ -12,7 +12,7 @@ import home_screen from './src/pages/home_screen';
 import create_acct_screen from './src/pages/create_acct_screen';
 import viewReview_screen from './src/pages/viewReview_screen';
 import viewRestaurant_screen from './src/pages/viewRestaurant_screen';
-//import viewSavedRestaurant_screen from './src/pages/viewSavedRestaurant_screen'
+import viewSavedRestaurant_screen from './src/pages/viewSavedRestaurant_screen'
 import editMenuPage from './src/pages/editMenuPage';
 import viewSavedRestaurant_screen from './src/pages/viewSavedRestaurant_screen';
 
@@ -24,7 +24,7 @@ export default function App() {
   return (
     <NavigationContainer>
           <Stack.Navigator initialRouteName="login_screen">
-            {/* <Stack.Screen name="home_screen" component={home_screen} options={{headerShown: false}} />
+            <Stack.Screen name="home_screen" component={home_screen} options={{headerShown: false}} />
             <Stack.Screen name="viewRestaurant_screen" component={viewRestaurant_screen} options={{headerShown: false}} />
             <Stack.Screen name="login_screen" component={login_screen} options={{headerShown: false}} />
             <Stack.Screen name="addRestaurant_screen" component={addRestaurant_screen} options={{headerShown: false}} />
@@ -32,7 +32,7 @@ export default function App() {
             <Stack.Screen name="viewMenu_screen" component={viewMenu_screen} options={{headerShown: false}} />
             <Stack.Screen name="create_acct_screen" component={create_acct_screen} options={{headerShown: false}} />
             <Stack.Screen name="viewSavedRestaurant_screen" component={viewSavedRestaurant_screen} options={{headerShown: false}} />
-            <Stack.Screen name='viewReview_screen' component={viewReview_screen} options={{headerShown: false}}/> */}
+            <Stack.Screen name='viewReview_screen' component={viewReview_screen} options={{headerShown: false}}/>
             <Stack.Screen name="editMenuPage" component={editMenuPage} options={{headerShown: false}} />
           </Stack.Navigator>
         </NavigationContainer>
